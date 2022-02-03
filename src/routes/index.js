2eee184efb0c3f8f0cd501eca06a5624cdfbc851
cezarBZ <cezar30santos@gmail.com>
2022-02-03 18:51:47 -0300

    criando as rotas

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+
+const CustomersController = require('../controllers/customers')
+//rotas 
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'titulo teste'
+    })
+})
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Cadastro de clientes'
+    })
+})
+
+router.post('/register/add', CustomersController.add)
+
+module.exports = router
