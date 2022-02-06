5e55ab9e50bf873439a0f6f7c0bb711f016959f5
cezarBZ <cezar30santos@gmail.com>
2022-02-06 19:21:51 -0300

    adcionando opção de editar cliente

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -2,12 +2,14 @@ const CustomersModel = require('../models/customers')
 const { crypto } = require('../utils/password')
 
 const defaultTitle = 'Cadastro de clientes'
+
+// renderiza a view 'register'
 function index(req, res)  {
     res.render('register', {
         title: defaultTitle
     })
 }
-
+//adciona usuário
 async function add(req, res) {
     const { name, age, email, password } = req.body
     
@@ -27,15 +29,48 @@ async function add(req, res) {
     })
 }
 
-async function listUsers(req,res) {
-    const users = await CustomersModel.find()
-    res.render('listUsers', {
+// lista usuário
+async function list(req,res) {
+    const users = await CustomersModel.find() // método do mongoose promise
+    res.render('list', {
         title: 'Listagem de usuários',
         users,
+    })
+}
+
+async function formEdit(req, res) {
+    const { id } = req.query
+
+    const user = await CustomersModel.findById(id)
+    res.render('edit',{
+        title: 'Editar Usuário',
+        user
+    })
+}
+
+async function edit(req, res){
+    const { name, age, email } = req.body
+
+    const { id } = req.params
+
+    const user = await CustomersModel.findById(id)
+
+
+    user.name = name
+    user.age = age
+    user.email = email
+
+    user.save()
+    res.render('edit', {
+        title: 'Editar usu´rio',
+        user,
+        message: 'Usuário alterado com sucesso'
     })
 }
 module.exports = {
     index,
     add,
-    listUsers
+    list,
+    formEdit,
+    edit
 }
